eb07ee507ba7537853833f787137e061de222a21
huengchi <10320321+huengchi@user.noreply.gitee.com>
2024-05-29 15:11:56 +0800

    fix bugs

--- toolkit/re_runner.py
+++ toolkit/re_runner.py
@@ -25,6 +25,15 @@ class MultiGPUTaskSchedulerReRunner:
         self._ifprint(f'[In Progress] Availabel GPU IDs: {self.available_gpu_ids}')
         self._ifprint(f'[In Progress] Collected failed tasks: {len(self.failed_inputs)}')
 
+        if '_rerun' in self.pipeline_execution_report_file:
+            serial = int(re.findall(r'_rerun(\d+)', self.pipeline_execution_report_file)[0])
+            serial += 1
+            report_file_path = re.sub(r'_rerun(\d+)', f"_rerun{serial}", self.pipeline_execution_report_file)
+        else:
+            name = self.pipeline_execution_report_file.split('.')
+            name.insert(1, '_rerun1.')
+            report_file_path = ''.join(name)
+
         if not dry_run:
             # emit sampling tasks
             for _,row in self.failed_inputs.iterrows():
@@ -35,14 +44,6 @@ class MultiGPUTaskSchedulerReRunner:
                 self.task_scheduler.emit_task(task_cmd=cmd, process_env=env, device_arg_option=None, setting_device_env=True)
 
             # Listen process execution progress
-            if '_rerun' in self.pipeline_execution_report_file:
-                serial = int(re.findall(r'_rerun(\d+)', self.pipeline_execution_report_file)[0])
-                serial += 1
-                report_file_path = re.sub(r'_rerun(\d+)', f"_rerun{serial}", self.pipeline_execution_report_file)
-            else:
-                name = self.pipeline_execution_report_file.split('.')
-                name.insert(1, '_rerun1.')
-                report_file_path = ''.join(name)
 
             pbar = tqdm(total=self.task_scheduler.total_task_cnt, disable=not self.verbose)
             for _ in range(self.task_scheduler.total_task_cnt):
@@ -51,11 +52,11 @@ class MultiGPUTaskSchedulerReRunner:
                 self.task_scheduler.report_progress().to_csv(report_file_path, index=False) # Progressively execution report
             pbar.close()
 
-            # No more tasks and inform to terminate
-            self.task_scheduler.notify_exit(wait_exiting=True)
-
             # Get final execution reports
             self.task_scheduler.report_progress().to_csv(report_file_path, index=False)
+
+        # No more tasks and inform to terminate
+        self.task_scheduler.notify_exit(wait_exiting=True)
 
         self._ifprint(f'[Finished] Execution report saved to {report_file_path}')
 
