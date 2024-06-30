ac70456b3cc072ce1d5ee26eb451ed0c827d8d73
huengchi <huengchi@smilelab.group>
2024-06-30 14:38:45 +0800

    minor change

--- toolkit/__init__.py
+++ toolkit/__init__.py
@@ -2,5 +2,6 @@ from .scheduler.multi_gpu_scheduler import MultiGPUTaskScheduler
 from .scheduler.re_runner import MultiGPUTaskSchedulerReRunner
 from .scheduler.runner import MultiGPUTaskSchedulerRunner
 from .file.pickle import read_pickle_file, write_pickle_file
+from .file.json import write_data_to_json_file
 from .subproc.run_w_retry import run_subprocess_with_retry
 from .argparse.type import str2bool, str2tuple
