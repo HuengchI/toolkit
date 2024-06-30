5b9e185a211e92c086c4e5f4ee1634d7d449db8b
huengchi <huengchi@smilelab.group>
2024-06-30 14:43:11 +0800

    minor changes

--- toolkit/file/json.py
+++ toolkit/file/json.py
@@ -1,10 +1,11 @@
 import json
 from typing import List, Dict, Union
 
-def write_data_to_json_file(data: Union[List|Dict], filepath):
+
+def write_data_to_json_file(data: Union[List|Dict], filepath, indent=4):
     try:
         with open(filepath, 'w') as file:
-            json.dump(data, file, indent=4)
+            json.dump(data, file, indent=indent)
         print(f'Data successfully written to {filepath}')
     except Exception as e:
         print(f'Error writing data to {filepath}: {e}')
